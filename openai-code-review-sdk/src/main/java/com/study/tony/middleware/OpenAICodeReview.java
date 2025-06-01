bac6eac43fa477fa681c99c229a0a9e55950e80e
Tony <1299351443@qq.com>
2025-06-01 21:33:30 +0800

    update code for init project

---
+++ openai-code-review-sdk/src/main/java/com/study/tony/middleware/OpenAICodeReview.java
@@ -0,0 +1,8 @@
+package com.study.tony.middleware;
+
+public class OpenAICodeReview {
+    public static void main(String[] args) {
+        System.out.println("OpenAI Code Review SDK is running successfully!");
+        // You can add more functionality here, such as initializing the SDK or performing code reviews.
+    }
+}
---
+++ openai-code-review-sdk/src/test/java/com/study/tony/middleware/ApiTest.java
@@ -0,0 +1,8 @@
+package com.study.tony.middleware;
+
+public class ApiTest {
+    public static void main(String[] args) {
+        System.out.println("Testing OpenAI Code Review SDK");
+        // You can add more functionality here, such as initializing the SDK or performing code reviews.
+    }
+}
