67a2eb0211736d943aa75a8c82ed22f01139b148
Tony <1299351443@qq.com>
2025-06-05 22:48:41 +0800

    20250605: xfg code refractor copy

--- docs/curl/curl-glm-4.sh
+++ docs/curl/curl-glm-4.sh
@@ -1,5 +1,5 @@
 curl -X POST \
-        -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04ifQ.eyJhcGlfa2V5IjoiZTk2NDI3ODVhMWYyNGNkMmFlNzYxODliN2QzZTIwY2MiLCJleHAiOjE3NTE2NzE1NTE1MjUsInRpbWVzdGFtcCI6MTc0OTA3OTU1MTUzMX0.A_yifnTsJBOc2eoa35PLturX5hCJ2vXlpYI1sY6ocd0" \
+        -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04ifQ.eyJhcGlfa2V5IjoiYzc4ZmJhY2QzZTEwMTE4YWQ1NjQ5ZDdhNTRhM2ExNjMiLCJleHAiOjE3MjIwNzczMTQxOTgsInRpbWVzdGFtcCI6MTcyMjA3NTUxNDIxMH0.CdDjJUs8XFUqZnjY08ffaoddLOtoCBn17vmttzkvM0I" \
         -H "Content-Type: application/json" \
         -H "User-Agent: Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)" \
         -d '{
--- openai-code-review-test/src/main/java/com/study/tony/middleware/Application.java
+++
@@ -1,8 +0,0 @@
-package com.study.tony.middleware;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("core review test runing!");
-        // You can add more functionality here, such as initializing the SDK or performing code reviews.
-    }
-}
